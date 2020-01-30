6a618d6fc5ee50254850ee45326806986ed1a4db
YellowPluff <denickel@widener.edu>
2020-01-29 19:26:00 -0500

    Added remove user and cleaned up code

--- app/src/main/java/com/example/networkingexample/MainActivity.java
+++ app/src/main/java/com/example/networkingexample/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -27,17 +28,19 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String first_name = "networkingapp.firstname";
-    public static final String last_name = "networkingapp.lastname";
-    public static final String major = "networkingapp.major";
+//    public static final String first_name = "networkingapp.firstname";
+//    public static final String last_name = "networkingapp.lastname";
+//    public static final String major = "networkingapp.major";
 
-    private EditText firstNameInputField;
-    private EditText lastNameInputField;
+    private EditText nameInputField;
+    private EditText idInputField;
     private Spinner majorSpinner;
-    private Button submitButton;
+    private Button addButton;
+    private Button removeButton;
+    private TextView textView;
 
     //User class is not created yet
-    List<User> users;
+//    List<User> users;
 
     DatabaseReference databaseUsers;
 
@@ -48,159 +51,178 @@ public class MainActivity extends AppCompatActivity {
 
         databaseUsers = FirebaseDatabase.getInstance().getReference("users");
 
-        firstNameInputField = (EditText) findViewById(R.id.firstNameField);
-        lastNameInputField = (EditText) findViewById(R.id.lastNameField);
-        majorSpinner = (Spinner) findViewById(R.id.majorSpinner);
+        nameInputField = findViewById(R.id.nameField);
+        idInputField = findViewById(R.id.idField);
+        majorSpinner = findViewById(R.id.majorSpinner);
+        addButton = findViewById(R.id.addButton);
+        removeButton = findViewById(R.id.deleteButton);
+        textView = findViewById(R.id.textView);
 
-        submitButton = (Button) findViewById(R.id.submitButton);
-        users = new ArrayList<>();
+//        users = new ArrayList<>();
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                String first_name = firstNameInputField.getText().toString().trim();
-                String last_name = lastNameInputField.getText().toString().trim();
+                String name = nameInputField.getText().toString().trim();
+                String studentId = idInputField.getText().toString().trim();
                 String major = majorSpinner.getSelectedItem().toString();
-                if(!first_name.isEmpty() && !last_name.isEmpty())
+                if(!name.isEmpty() && !studentId.isEmpty())
                 {
-                    String displayString = "Hello, " + first_name + " " + last_name + ". Major: " + major;
-                    Snackbar.make(view, displayString, Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                    addUser();
+                    textView.setText("Hello, " + name + " (" + studentId + "). Major: " + major);
+                    int returnCode = addUser(name, studentId, major); //TODO
+                    if(returnCode == 0)
+                    {
+                        //Resetting input fields
+                        nameInputField.setText("");
+                        idInputField.setText("");
+
+                        //displaying a success toast
+                        textView.setText("User added successfully");
+                    }
                 }
                 }
             });
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String studentId = idInputField.getText().toString().trim();
+                if(!studentId.isEmpty())
+                {
+                    int returnCode = deleteUser(studentId);
+                    if(returnCode == 0)
+                    {
+                        //Resetting input fields
+                        nameInputField.setText("");
+                        idInputField.setText("");
+
+                        //displaying a success toast
+                        textView.setText("User Deleted successfully");
+                    }
+                }
+            }
+        });
         }
 
-        /*listViewUsers.setOnItemClickListener(new AdapterView.OnItemClickListener()) {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //getting the selected user
-                //User user = users.get(i);
+    /*
+     * This method is saving a new User to the Firebase Realtime Database
+     * */
+    private int addUser(String name, String studentId, String major) {
+        //getting a unique id using push().getKey() method
+        //it will create a unique id and we will use it as the Primary Key for our User
+        String id = databaseUsers.push().getKey();
 
-                //creating an intent
-                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+        //creating an User Object
+        User user = new User(name, studentId, major);
 
-                //putting artist name and id to intent
-                intent.putExtra(first_name, firstNameInputField.getText().toString());
-                intent.putExtra(last_name, lastNameInputField.getText().toString());
-                intent.putExtra(major, majorSpinner.getSelectedItem().toString());
-                //starting the activity with intent
-                startActivity(intent);
-            }
-        });
-
-        listViewUsers.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                User user = users.get(i);
-                showUpdateDeleteDialog(user.getUserId(), user.getUserName());
-                return true;
-            }
-        });
-
-    }*/
-    /*private void showUpdateDeleteDialog(final String artistId, String artistName) {
-
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
-        dialogBuilder.setView(dialogView);
-
-
-
-        dialogBuilder.setTitle(userName);
-        final AlertDialog b = dialogBuilder.create();
-        b.show();
-
+        databaseUsers.child(id).setValue(user);
+        return 0;
     }
 
-    private boolean deleteUser(String id) {
-        //getting the specified artist reference
+    /*
+     * This method is removing a User from the Firebase Realtime Database
+     */
+    private int deleteUser(String id) {
+        //getting the specified User reference
+        //TODO I'm not sure what "users" references
         DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(id);
 
-        //removing artist
+        //removing User
         dR.removeValue();
 
-        //getting the tracks reference for the specified artist
+        //getting the tracks reference for the specified User
         DatabaseReference drTracks = FirebaseDatabase.getInstance().getReference("tracks").child(id);
 
         //removing all tracks
         drTracks.removeValue();
-        Toast.makeText(getApplicationContext(), "User Deleted", Toast.LENGTH_LONG).show();
 
-        return true;
-    }*/
-
-    /*@Override
-    protected void onStart() {
-        super.onStart();
-        //attaching value event listener
-        databaseArtists.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                //clearing the previous artist list
-                users.clear();
-
-                //iterating through all the nodes
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    //getting artist
-                    User user = postSnapshot.getValue(user.class);
-                    //adding artist to the list
-                    users.add(user);
-                }
-
-                //creating adapter
-                UserList userAdapter = new UserList(MainActivity.this, users);
-                //attaching adapter to the listview
-                listViewUsers.setAdapter(userAdapter);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });*/
-
-
-
-    /*
-     * This method is saving a new artist to the
-     * Firebase Realtime Database
-     * */
-    private void addUser() {
-        //getting the values to save
-        String firstname = firstNameInputField.getText().toString().trim();
-        String lastname = lastNameInputField.getText().toString().trim();
-        String major = majorSpinner.getSelectedItem().toString();
-        String name = firstname + " " + lastname;
-
-        //checking if the value is provided
-        if (!TextUtils.isEmpty(name)) {
-
-            //getting a unique id using push().getKey() method
-            //it will create a unique id and we will use it as the Primary Key for our Artist
-            String id = databaseUsers.push().getKey();
-
-            //creating an User Object
-            //User user = new User(firstname, lastname, major);
-
-            //Saving the Artist
-            User user = new User();
-            user.firstName = firstname;
-            user.lastName = lastname;
-            user.major = major;
-            databaseUsers.child(id).setValue(user);
-            //setting edittext to blank again
-            firstNameInputField.setText("");
-
-            //displaying a success toast
-            Toast.makeText(this, "User added", Toast.LENGTH_LONG).show();
-        } else {
-            //if the value is not given displaying a toast
-            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
-        }
+        return 0;
     }
+
+
+
+
+
+
+
+
+
+
+
+    
+
+//        listViewUsers.setOnItemClickListener(new AdapterView.OnItemClickListener()) {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                //getting the selected user
+//                //User user = users.get(i);
+//
+//                //creating an intent
+//                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+//
+//                //putting User name and id to intent
+//                intent.putExtra(first_name, firstNameInputField.getText().toString());
+//                intent.putExtra(last_name, lastNameInputField.getText().toString());
+//                intent.putExtra(major, majorSpinner.getSelectedItem().toString());
+//                //starting the activity with intent
+//                startActivity(intent);
+//            }
+//        });
+
+//        listViewUsers.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                User user = users.get(i);
+//                showUpdateDeleteDialog(user.getUserId(), user.getUserName());
+//                return true;
+//            }
+//        });
+
+//    private void showUpdateDeleteDialog(final String userId, String userName) {
+//
+//        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+//        LayoutInflater inflater = getLayoutInflater();
+//        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+//        dialogBuilder.setView(dialogView);
+//
+//
+//
+//        dialogBuilder.setTitle(userName);
+//        final AlertDialog b = dialogBuilder.create();
+//        b.show();
+//
+//    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        //attaching value event listener
+//        databaseArtists.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                //clearing the previous User list
+//                users.clear();
+//
+//                //iterating through all the nodes
+//                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                    //getting User
+//                    User user = postSnapshot.getValue(user.class);
+//                    //adding User to the list
+//                    users.add(user);
+//                }
+//
+//                //creating adapter
+//                UserList userAdapter = new UserList(MainActivity.this, users);
+//                //attaching adapter to the listview
+//                listViewUsers.setAdapter(userAdapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
 
 }
--- app/src/main/java/com/example/networkingexample/User.java
+++ app/src/main/java/com/example/networkingexample/User.java
@@ -1,9 +1,15 @@
 package com.example.networkingexample;
 
 public class User {
-    public String firstName;
-    public String lastName;
+    public String name;
+    public String studentId;
     public String major;
 
+    public User(String name, String studentId, String major)
+    {
+        this.name = name;
+        this.studentId = studentId;
+        this.major = major;
+    }
 
 }
